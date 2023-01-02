f6ae2b8db1adc10c238cb504259d961f28ac0dad
rhythmicrhythm <Okubadejorh@gmail.com>
2023-01-02 20:03:38 +0100

    Register Route connected and working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/auth/Login";
-
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Reset from "./pages/auth/Reset";
 // import Dashboard from "./pages/dashboard/Dashboard";
+import AddProduct from "./pages/addProduct/AddProduct";
 import Sidebar from "./components/sidebar/Sidebar";
 import Layout from "./components/layout/Layout";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <BrowserRouter>
+     <ToastContainer />
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot" element={<Forgot />} />
+      <Route path="/resetpassword/:resetToken" element={<Reset />} />
 
       <Route
           path="/dashboard"
@@ -24,6 +38,68 @@ function App() {
             </Sidebar>
           }
         />
+
+       <Route
+          path="/add-product"
+          element={
+            <Sidebar>
+              <Layout>
+                {/* <AddProduct /> */}
+              </Layout>
+            </Sidebar>
+          }
+        />
+        {/* <Route
+          path="/product-detail/:id"
+          element={
+            <Sidebar>
+              <Layout>
+                <ProductDetail />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/edit-product/:id"
+          element={
+            <Sidebar>
+              <Layout>
+                <EditProduct />
+              </Layout>
+            </Sidebar>
+          }
+        /> */}
+        {/* <Route
+          path="/profile"
+          element={
+            <Sidebar>
+              <Layout>
+                <Profile />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/edit-profile"
+          element={
+            <Sidebar>
+              <Layout>
+                <EditProfile />
+              </Layout>
+            </Sidebar>
+          }
+        />
+        <Route
+          path="/contact-us"
+          element={
+            <Sidebar>
+              <Layout>
+                <Contact />
+              </Layout>
+            </Sidebar>
+          }
+        /> */}
+
       </Routes>
     </BrowserRouter>
   );
