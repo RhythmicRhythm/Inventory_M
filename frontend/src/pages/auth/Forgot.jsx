16df1ab6a0267b3f890e83f2c25d5d56cca3946b
rhythmicrhythm <Okubadejorh@gmail.com>
2023-01-04 23:43:55 +0100

    forgot password and reset password route now working

--- frontend/src/pages/auth/Forgot.jsx
+++ frontend/src/pages/auth/Forgot.jsx
@@ -3,29 +3,29 @@ import styles from "./auth.module.scss";
 import { AiOutlineMail } from "react-icons/ai";
 import Card from "../../components/card/Card";
 import { Link } from "react-router-dom";
-// import { forgotPassword, validateEmail } from "../../services/authService";
+import { forgotPassword, validateEmail } from "../../services/authService";
 import { toast } from "react-toastify";
 
 const Forgot = () => {
-  // const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
 
-  // const forgot = async (e) => {
-  //   e.preventDefault();
-  //   if (!email) {
-  //     return toast.error("Please enter an email");
-  //   }
+  const forgot = async (e) => {
+    e.preventDefault();
+    if (!email) {
+      return toast.error("Please enter an email");
+    }
 
-  //   if (!validateEmail(email)) {
-  //     return toast.error("Please enter a valid email");
-  //   }
+    if (!validateEmail(email)) {
+      return toast.error("Please enter a valid email");
+    }
 
-  //   const userData = {
-  //     email,
-  //   };
+    const userData = {
+      email,
+    };
 
-  //   await forgotPassword(userData);
-  //   setEmail("");
-  // };
+    await forgotPassword(userData);
+    setEmail("");
+  };
 
   return (
     <div className={`container ${styles.auth}`}>
@@ -37,15 +37,15 @@ const Forgot = () => {
           <h2>Forgot Password</h2>
 
           <form 
-          // onSubmit={forgot}
+          onSubmit={forgot}
           >
             <input
               type="email"
               placeholder="Email"
               required
               name="email"
-              // value={email}
-              // onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <button type="submit" className="--btn --btn-primary --btn-block">
